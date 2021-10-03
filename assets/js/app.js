b3b8ef55121e2a4f083e3ab50a2da33258774df4
yacn1214 <zy11hey@gmail.com>
2021-10-03 22:40:44 +0330

    one Release

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+//variable
+const noteList = document.querySelector('#note-list')
+
+//eventlisteners
+eventlisteners()
+function eventlisteners() {
+	document.querySelector('#form').addEventListener('submit',newNote)
+}
+//functions
+function newNote(e) {
+	e.preventDefault()
+
+	// access to the value
+	const note = document.querySelector('#note').value
+
+	// create remove element
+	const removebtn = document.createElement('a')
+	removebtn.textContent="X"
+	removebtn.classList='remove-note'
+	
+	// Create tag <li> 
+	const li =document.createElement('li')
+	li.appendChild(document.createTextNode(note))
+	
+	//adding remove button to the li
+	li.appendChild(removebtn)
+	removebtn.onclick= function() {
+		li.remove()
+	}
+	//adding li to the note-list
+	noteList.appendChild(li)
+}
+let go = document.getElementById('del')
+go.onclick=function() {
+	noteList.remove()
+}
